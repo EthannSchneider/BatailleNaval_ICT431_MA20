0c9e61b353b86634e3bd3b7413eb7adc81847060
SCHNEIDER Ethann <Ethann.SCHNEIDER@cpnv.ch>
2021-03-01 13:51:32 +0100

    BatailleNaval_ICT431_MA20

--- cmake-build-debug/CMakeFiles/BatailleNaval_ICT431_MA20.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/BatailleNaval_ICT431_MA20.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_C
-  "C:/Users/Ethann.SCHNEIDER/CLionProjects/BatailleNaval_ICT431_MA20/main.c" "C:/Users/Ethann.SCHNEIDER/CLionProjects/BatailleNaval_ICT431_MA20/cmake-build-debug/CMakeFiles/BatailleNaval_ICT431_MA20.dir/main.c.obj"
+  "C:/dev/BatailleNaval_ICT431_MA20/main.c" "C:/dev/BatailleNaval_ICT431_MA20/cmake-build-debug/CMakeFiles/BatailleNaval_ICT431_MA20.dir/main.c.obj"
   )
 set(CMAKE_C_COMPILER_ID "GNU")
 
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Ethann.SCHNEIDER/CLionProjects/BatailleNaval_ICT431_MA20
+# Install script for directory: C:/dev/BatailleNaval_ICT431_MA20
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Ethann.SCHNEIDER/CLionProjects/BatailleNaval_ICT431_MA20/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/dev/BatailleNaval_ICT431_MA20/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
