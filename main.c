8dce14225ba63ab43f1200b89c69b97d949c94b1
EthannSchneider <ethann.schneider@cpnv.ch>
2021-03-02 22:20:52 +0100

    project7

--- main.c
+++ main.c
@@ -8,6 +8,9 @@ Version : beta 0.1
 #include <stdlib.h>
 #include <time.h>
 
+int x = 0;
+int y = 0;
+
 //1 : 1 porte-avion (5 cases)
 //2 : 1 croiseur (4 cases)
 //3 : 1 contre-torpilleur (3 cases)
@@ -44,7 +47,6 @@ int tab[10][10] = {
 };
 
 int tableau(){
-    printf("\e[1;1H\e[2J");
     printf("     1   2   3   4   5   6   7   8   9  10\n");
     printf("   ╔═══╦═══╦═══╦═══╦═══╦═══╦═══╦═══╦═══╦═══╗\n %c ║",97);
     for (int i = 0; i < 10; i++) {
@@ -62,7 +64,45 @@ int tableau(){
 }
 
 int main() {
-    tableau();
+    char ychar;
+    while (1){
+        printf("\e[1;1H\e[2J");
+        tableau();
+
+        do {
+            printf("\nCoords 1 : ");
+            scanf("%d", &x);
+            if (x > 10 || x == 0){printf("le nombre doit être inférieur à 10 et supérieur a 0 !");}
+        }while (x > 10 || x == 0);
+
+        do {
+            printf("\nCoords 2 : ");
+            scanf("%s", &ychar);
+            y = (int) (ychar) - 96;
+            if (ychar <= 96 || ychar >= 107){printf("La lettre doit être entre a et k !");}
+        } while (ychar <= 96 || ychar >= 107);
+
+        switch (bateau[x-1][y-1]) {
+            case 0:
+                tab[x-1][y-1] = 1;
+                break;
+            case 1:
+                tab[x-1][y-1] = 2;
+                break;
+            case 2:
+                tab[x-1][y-1] = 2;
+                break;
+            case 3:
+                tab[x-1][y-1] = 2;
+                break;
+            case 4:
+                tab[x-1][y-1] = 2;
+                break;
+
+
+        }
+
+    }
 
     return 0;
 }
