f21dc0f59666c4f480f82c7c0c4486d56cece04d
SCHNEIDER Ethann <Ethann.SCHNEIDER@cpnv.ch>
2021-03-03 10:01:44 +0100

    add BatailleNaval_ICT431_MA20

--- cmake-build-debug/CMakeFiles/BatailleNaval_ICT431_MA20.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/BatailleNaval_ICT431_MA20.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_C
-  "/home/ethann/Work/C/BatailleNaval_ICT431_MA20/main.c" "/home/ethann/Work/C/BatailleNaval_ICT431_MA20/cmake-build-debug/CMakeFiles/BatailleNaval_ICT431_MA20.dir/main.c.o"
+  "C:/dev/BatailleNaval_ICT431_MA20/main.c" "C:/dev/BatailleNaval_ICT431_MA20/cmake-build-debug/CMakeFiles/BatailleNaval_ICT431_MA20.dir/main.c.obj"
   )
 set(CMAKE_C_COMPILER_ID "GNU")
 
--- cmake-build-debug/CMakeFiles/BatailleNaval_ICT431_MA20.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/BatailleNaval_ICT431_MA20.dir/cmake_clean.cmake
@@ -1,7 +1,9 @@
 file(REMOVE_RECURSE
-  "BatailleNaval_ICT431_MA20"
+  "BatailleNaval_ICT431_MA20.exe"
+  "BatailleNaval_ICT431_MA20.exe.manifest"
   "BatailleNaval_ICT431_MA20.pdb"
-  "CMakeFiles/BatailleNaval_ICT431_MA20.dir/main.c.o"
+  "CMakeFiles/BatailleNaval_ICT431_MA20.dir/main.c.obj"
+  "libBatailleNaval_ICT431_MA20.dll.a"
 )
 
 # Per-language clean rules from dependency scanning.
