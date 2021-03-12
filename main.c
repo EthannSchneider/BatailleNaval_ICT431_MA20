02d1d502c6e072aa432d4bc149b3992d220339aa
EthannSchneider <ethann.schneider@cpnv.ch>
2021-03-12 14:41:29 +0100

    BatailleNavale_ICT431_MA20v1.0

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 /*
 Name : Bataille naval
 Author : Ethann Schneider
-Version : beta 0.1
+Version : beta 1.0
 */
 
 #include <stdio.h>
@@ -60,6 +60,31 @@ int tab[10][10] = {
         {0,0,0,0,0,0,0,0,0,0},
         {0,0,0,0,0,0,0,0,0,0}
 };
+
+/*@function: write in log file
+ *@param: Text to write in log
+ * */
+void BatNavlog(const char *BatNavlog,char *){
+    fichier = fopen("log.txt", "a+");
+
+    time_t now;
+    time(&now);
+    struct tm *local = localtime(&now);
+    hours = local->tm_hour;          // get hours since midnight (0-23)
+    minutes = local->tm_min;         // get minutes passed after the hour (0-59)
+    seconds = local->tm_sec;         // get seconds passed after minute (0-59)
+
+    day = local->tm_mday;            // get day of month (1 to 31)
+    month = local->tm_mon + 1;       // get month of year (0 to 11)
+    year = local->tm_year + 1900;    // get year since 1900
+
+    if (fichier != NULL)
+    {
+        fprintf(fichier,"[%02d/%02d/%d %02d:%02d:%02d]: %s\n", day, month, year,hours, minutes, seconds, BatNavlog);
+    }
+
+    fclose(fichier);
+}
 
 /* @function : transform number to char on the graphic table
 */
